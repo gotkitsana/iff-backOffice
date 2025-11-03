c5cd08b06cc5a6de76a9d2a0b1426c3d7d6c91f6
gotkitsana <gotgotzx@hotmail.com>
2025-11-03 13:44:36 +0700

    Enhance form validation in ModalEditProduct to allow 'balance' field to bypass required checks when it has a value
    - Updated validation logic to ensure that the 'balance' field is treated differently, improving user experience by allowing it to pass validation if a value is present.

--- src/components/product/modal/ModalEditProduct.vue
+++ src/components/product/modal/ModalEditProduct.vue
@@ -201,6 +201,9 @@ const validateForm = () => {
 
   // Validate required fields
   for (const field of selectedCategoryInfo.value.fields) {
+    if (field.key === 'balance' && dynamicFormData.value![field.key] != null) {
+      return true
+    }
     if (field.required && !dynamicFormData.value![field.key]) {
       toast.error(`กรุณากรอก${field.label}`)
       return false
