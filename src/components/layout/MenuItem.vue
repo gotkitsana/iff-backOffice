47fcb95235df63c5b3b7c02ff3daeebcd00ca6b7
gotkitsana <gotgotzx@hotmail.com>
2025-11-18 09:58:18 +0700

    Implement Dropdown Menu with Slide-Fade Transition in Sidebar
    - Added slide-fade transition styles for dropdown menus in style.css.
    - Refactored Sidebar.vue to include a dropdown menu for sales, allowing for better organization of submenu items.
    - Introduced logic to auto-open the dropdown based on the current route, enhancing user navigation experience.
    - Updated MenuItem.vue to support submenu items with appropriate styling and active state management.

--- src/components/layout/MenuItem.vue
+++ src/components/layout/MenuItem.vue
@@ -3,20 +3,24 @@
     @click="$emit('click')"
     :class="[
       'w-full flex items-center space-x-3 px-3 py-1.5 text-sm rounded-full transition-all duration-200 group',
-      active && 'bg-white lg:drop-shadow-xs ',
-      submenu ? 'ml-6 text-xs py-2' : '',
+      active && !submenu && 'bg-white lg:drop-shadow-xs ',
+      submenu ? 'ml-2 text-xs py-2' : '',
     ]"
   >
     <i
       :class="[
         icon,
         'text-sm ',
-        active ? 'text-blue-500 lg:text-black' : 'text-gray-600 group-hover:text-black',
+        active && submenu && '!text-blue-600',
+        active && !submenu ? 'text-blue-500 lg:text-black' : 'text-gray-600 group-hover:text-black',
       ]"
     ></i>
     <span
       class="flex-1 text-left font-[500]!"
-      :class="active ? 'text-blue-500 lg:text-black' : 'text-gray-600 group-hover:text-black'"
+      :class="[
+        active && submenu && '!text-blue-600 font-semibold!',
+        active && !submenu ? 'text-blue-500 lg:text-black' : 'text-gray-600 group-hover:text-black',
+      ]"
       >{{ label }}</span
     >
   </button>
